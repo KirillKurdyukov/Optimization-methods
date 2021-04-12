e90e3e7efe6eca64d50fae98cb57c9bb9c128dc1
Кирилл Курдюков <kurdyukov_kirya@mail.ru>
2021-04-12 18:55:36 +0300

    Pasha ebi

--- Lab2/methods/GradientDescent.java
+++ Lab2/methods/GradientDescent.java
@@ -11,27 +11,15 @@ public class GradientDescent {
                              VectorNumbers x,
                              Function<VectorNumbers, Double> function) {
         VectorNumbers y;
-        while(gradient.module(x) >= eps) {
+        while(gradient.module(x) > eps) {
             y = gradient.evaluate(x).multiplyConst(-1 * alpha).add(x);
             while(function.apply(y) >= function.apply(x)) {
                 alpha = alpha / 2;
-                y = gradient.evaluate(x).multiplyConst(-1 * alpha).add(x);
+                y = gradient.evaluate(x).multiplyConst(-1 * alpha / gradient.module(x)).add(x);
             }
             x = y;
         }
         return function.apply(x);
     }
 
-    public static void main(String[] args) {
-        Function<VectorNumbers, Double> function = (VectorNumbers x) ->
-                (x.get(0) - 1) * (x.get(0) - 1) + x.get(1) * x.get(1);
-        Function<VectorNumbers, Double> dx = (VectorNumbers x) ->
-                x.get(0) * 2 - 2;
-        Function<VectorNumbers, Double> dy = (VectorNumbers x) ->
-                x.get(1) * 2;
-        Gradient gradient = new Gradient(List.of(dx, dy));
-        VectorNumbers x = new VectorNumbers(List.of(200.0, 200.0));
-
-        System.out.println(run(0.00000001, 16, gradient, x, function));
-    }
 }
---
+++ Lab2/methods/OneDimensionalOptimization.java
@@ -0,0 +1,233 @@
+package methods;
+
+import processing.Trie;
+
+import java.util.ArrayList;
+import java.util.function.Function;
+
+public class OneDimensionalOptimization {
+
+    /**
+     * Метод дихотомии.
+     * Алгоритм работает корректно на интервале [l, r], если
+     * функция унимодальная на этом отрезке.
+     * Дельта берется EPS / 2.
+     * Значение для ответа берется из середины отрезка [l; r], когда границы
+     * не различимы первыми 4 значащими цифрами после запятой.
+     */
+    public static Double methodDichotomy(Function<Double, Double> function,
+                                         double l,
+                                         double r,
+                                         double eps) {
+        double delta = eps / 2, x1, x2;
+        while ((r - l) / 2 > eps) {
+            x1 = (l + r) / 2 - delta;
+            x2 = (l + r) / 2 + delta;
+            if (function.apply(x1) <= function.apply(x2)) {
+                r = x2;
+            } else {
+                l = x1;
+            }
+        }
+        return (r + l) / 2;
+    }
+
+    /**
+     * Метод золотого сечения.
+     * Алгоритм работает корректно на интервале [l, r], если
+     * функция унимодальная на этом отрезке.
+     * За константу Ф берется пропорция золотого сечения.
+     * Граница высчитывается один раз.
+     * Значение для ответа берется из середины отрезка [l < x1; x2 < r], когда границы l, r
+     * не различимы первыми 4 значащими цифрами после запятой.
+     */
+    public static Double methodGoldenRatio(Function<Double, Double> function,
+                                           double l,
+                                           double r,
+                                           double eps) {
+        final double phi = (1 + Math.sqrt(5)) / 2;
+        final double resPhi = 2 - phi;
+        double x1 = l + resPhi * (r - l),
+                x2 = r - resPhi * (r - l),
+                f1Val = function.apply(x1), f2Val = function.apply(x2);
+        while ((r - l) > eps) {
+            if (f1Val < f2Val) {
+                r = x2;
+                x2 = x1;
+                f2Val = f1Val;
+                x1 = l + resPhi * (r - l);
+                f1Val = function.apply(x1);
+            } else {
+                l = x1;
+                x1 = x2;
+                f1Val = f2Val;
+                x2 = r - resPhi * (r - l);
+                f2Val = function.apply(x2);
+            }
+        }
+        return function.apply((l + r) / 2);
+    }
+
+    /**
+     * Метод Фибоначчи.
+     * Улучшение реализации "поиска с помощью золотого сечения", так
+     * как высчитывание коэффициентов происходит в типах Long, а Double.
+     * Вычисления вспомогательного множителя происходит с помощью чисел Фибоначчи.
+     * Алгоритм работает корректно на интервале [l, r], если
+     * функция унимодальная на этом отрезке.
+     * Значение для ответа берется из середины отрезка [l < x1; x2 < r], когда границы l, r
+     * не различимы первыми 4 значащими цифрами после запятой.
+     */
+    public static Double methodFibonacciNumbers(Function<Double, Double> function,
+                                                double l,
+                                                double r,
+                                                double eps) {
+        long n = (long) ((r - l) / eps);
+        ArrayList<Long> fib = new ArrayList<>();
+        fib.add(0L);
+        fib.add(1L);
+        int s = -1;
+        while (n > fib.get(fib.size() - 1)) {
+            fib.add(fib.get(fib.size() - 1) + fib.get(fib.size() - 2));
+            s++;
+        }
+        double cur = (double) fib.get(s - 1) / fib.get(s) * (r - l);
+        double x2 = l + cur;
+        double x1 = r - cur;
+        double f1 = function.apply(x1), f2 = function.apply(x2);
+        for (int i = s - 1; i > 2; i--) {
+            if (f1 < f2) {
+                r = x2;
+                x2 = x1;
+                f2 = f1;
+                x1 = l + (double) fib.get(i - 2) / fib.get(i) * (r - l);
+                f1 = function.apply(x1);
+            } else {
+                l = x1;
+                x1 = x2;
+                f1 = f2;
+                x2 = l + (double) fib.get(i - 1) / fib.get(i) * (r - l);
+                f2 = function.apply(x2);
+            }
+        }
+        return function.apply((l + r) / 2);
+    }
+
+    /**
+     * Метод парабол
+     */
+    public static Double methodParabolas(Function<Double, Double> function,
+                                         double l,
+                                         double r,
+                                         double eps) {
+        double fl = function.apply(l);
+        double fr = function.apply(r);
+        double x = (l + r) / 2;
+        double fx = function.apply(x);
+        while (r - l > eps) {
+            double u = x - (Math.pow(x - l, 2) * (fx - fr) - Math.pow(x - r, 2) * (fx - fl)) / (2 * ((x - l) * (fx - fr) - (x - r) * (fx - fl)));
+            double fu = function.apply(u);
+            if (fu > fx) {
+                if (u > x) {
+                    r = u;
+                    fr = fu;
+                } else {
+                    l = u;
+                    fl = fu;
+                }
+            } else {
+                if (x > u) {
+                    r = x;
+                    fr = fx;
+                } else {
+                    l = x;
+                    fl = fx;
+                }
+                x = u;
+                fx = fu;
+            }
+        }
+        return function.apply((l + r) / 2);
+    }
+
+    /**
+        метод Брента.
+     */
+    public static Double methodBrent(Function<Double, Double> function,
+                               double l,
+                               double r,
+                               double eps) {
+        double k = (3 - Math.sqrt(5)) / 2;
+        double x, w, v;
+        double a = l;
+        double c = r;
+        x = w = v = (a + c) / 2;
+        double fx, fw, fv;
+        fx = fw = fv = function.apply(x);
+        double d, e;
+        d = e = c - a;
+        while (d > eps) {
+            double g;
+            g = e;
+            e = d;
+            double u;
+            if (!(fx == fw || fx == fv || fv == fw)) {
+                u = x - (Math.pow((x - w), 2) * (fx - fv) - Math.pow((x - v), 2) * (fx - fw)) / (2 * ((x - w) * (fx - fv) - (x - v) * (fx - fw)));
+                if (u >= a + eps && u <= c - eps && Math.abs(u - x) < g / 2) {
+                    d = Math.abs(u - x);
+                } else {
+                    if (x < (c - a) / 2) {
+                        u = a + k * (c - x);
+                        d = c - x;
+                    } else {
+                        u = c - k * (x - a);
+                        d = x - a;
+                    }
+                }
+            } else {
+                if (x < (c - a) / 2) {
+                    u = x + k * (c - x);
+                    d = c - x;
+                } else {
+                    u = x - k * (x - a);
+                    d = x - a;
+                }
+                if (Math.abs(u - x) < eps) {
+                    u = x + Math.signum(u - x) * eps;
+                }
+            }
+            double fu = function.apply(u);
+            if (fu <= fx) {
+                if (u >= x) {
+                    a = x;
+                } else {
+                    c = x;
+                }
+                v = w;
+                w = x;
+                x = u;
+                fv = fw;
+                fw = fx;
+                fx = fu;
+            } else {
+                if (u >= x) {
+                    c = u;
+                } else {
+                    a = u;
+                }
+                if (fu <= fw || w == x) {
+                    v = w;
+                    w = u;
+                    fv = fw;
+                    fw = fu;
+                } else {
+                    if (fu <= fv || v == x || v == w) {
+                        v = u;
+                        fv = fu;
+                    }
+                }
+            }
+        }
+        return function.apply((a + c) / 2);
+    }
+}
