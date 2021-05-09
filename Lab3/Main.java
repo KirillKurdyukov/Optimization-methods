3aebc0ba3a629f1f4c1bd7415a765d2388a65ba8
Кирилл Курдюков <kurdyukov_kirya@mail.ru>
2021-05-09 20:20:28 +0300

    gayss:

--- Lab3/Main.java
+++ Lab3/Main.java
@@ -64,7 +64,7 @@ public class Main {
                     return 0;
         }
         for (int row = 0; row < size; row++)
-            if (checkIncomplatibleLine(matrix, row))
+            if (checkIncompatibleLine(matrix, row))
                 return 0;
         for (int row = 0; row < size; row++) {
             if (checkManySolution(matrix, row))
@@ -78,7 +78,23 @@ public class Main {
         return 2;
     }
 
-    boolean pivoting(Matrix matrix, int row, int col) {
+    void madeDiagView(Matrix matrix, int col) {
+        for (int i = col - 1; i >= 0; i--) {
+            double delta = matrix.get(i, col) / matrix.get(col, col);
+            matrix.set(i, col, matrix.get(i, col) - delta * matrix.get(col, col));
+            matrix.setFreeVectorNum(i, matrix.getFreeVector(i) - delta * matrix.getFreeVector(col));
+        }
+    }
+
+    private boolean checkManySolution(Matrix matrix, int row) {
+        int size = matrix.size();
+        for (int i = 0; i < size; i++) {
+            if (Math.abs(matrix.get(row, i)) > eps)
+                return false;
+        }
+        return true;
+    }
+    private boolean pivoting(Matrix matrix, int row, int col) {
         int maxLineIndex = row;
         double localMax = 0.0;
         for (int i = row; i < matrix.size(); i++) {
@@ -93,7 +109,7 @@ public class Main {
         return true;
     }
 
-    boolean madeTriangularView(Matrix matrix, int row, int col) {
+    private boolean madeTriangularView(Matrix matrix, int row, int col) {
         int size = matrix.size();
         for (int i = row + 1; i < size; i++) {
             double delta = matrix.get(i, col) / matrix.get(row, col);
@@ -101,21 +117,31 @@ public class Main {
                 matrix.set(i, j,  matrix.get(i, j) - delta * matrix.get(row, j));
             matrix.setFreeVectorNum(i, matrix.getFreeVector(i) - delta * matrix.getFreeVector(row));
             boolean checkZeroLine = true;
-            for (size_t j = col; j < size; j++) {
-                if (fabs(linSystem[i][j]) > eps)
+            for (int j = col; j < size; j++) {
+                if (Math.abs(matrix.get(i, j)) > eps)
                     checkZeroLine = false;
             }
             if (checkZeroLine) {
-                if (fabs(linSystem[i][size]) > eps)
+                if (Math.abs(matrix.getFreeVector(i)) > eps)
                     return true;
             }
             if (checkZeroLine) {
-                for (size_t j = 0; j < size + 1; j++) {
-                    linSystem[i][j] = 0.0;
+                for (int j = 0; j < size; j++) {
+                    matrix.set(i, j, 0.0);
                 }
+                matrix.setFreeVectorNum(i, 0.0);
             }
         }
         return false;
     }
 
+
+    private boolean checkIncompatibleLine(Matrix matrix, int row) {
+        int size = matrix.size();
+        for (int i = 0; i < size; i++) {
+            if (Math.abs(matrix.get(row, i)) > eps)
+                return false;
+        }
+        return !(Math.abs(matrix.get(row, size)) <= eps);
+    }
 }
