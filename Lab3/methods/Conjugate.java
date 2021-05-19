13e3a8e42ec79ddb495cd7a67d66ec9eb8ed7a93
BetsonWick <strelnikovila@gmail.com>
2021-05-19 14:24:36 +0300

    s

--- Lab3/methods/Conjugate.java
+++ Lab3/methods/Conjugate.java
@@ -10,44 +10,64 @@ import java.util.stream.IntStream;
 public class Conjugate extends TestAbstract {
     public static int lastIterations;
 
-    double[] subtractVectors(double[] a, double[] b) {
+    double[] mult(SparseMatrix A, double[] vect) {
+        double[] res = new double[A.getColumnNumbers()];
+        for (int i = 0; i < A.getColumnNumbers(); i++) {
+            for (int j = 0; j < A.getColumnNumbers(); j++) {
+                res[i] += A.getElement(i, j) * vect[j];
+            }
+        }
+        return res;
+    }
+
+    double[] subtract(double[] a, double[] b) {
         double[] res = Arrays.copyOf(a, a.length);
-        IntStream.range(0, a.length).forEach(i -> res[i] -= b[i]);
+        for (int i = 0; i < a.length; i++) {
+            res[i] -= b[i];
+        }
         return res;
     }
 
     double scalar(double[] a, double[] b) {
-        return IntStream.range(0, a.length).mapToDouble(i -> a[i] * b[i]).sum();
-    }
-
-    double[] sumVectors(double[] a, double[] b) {
-        double[] res = new double[a.length];
-        IntStream.range(0, a.length).forEach(i -> res[i] = b[i] + a[i]);
+        double res = 0;
+        for (int i = 0; i < a.length; i++) {
+            res += a[i] * b[i];
+        }
         return res;
     }
 
-    double[] multiply(double alpha, double[] b) {
+    double[] sum(double[] a, double[] b) {
+        double[] res = new double[a.length];
+        for (int i = 0; i < a.length; i++) {
+            res[i] = b[i] + a[i];
+        }
+        return res;
+    }
+
+    double[] mult(double alpha, double[] b) {
         double[] res = new double[b.length];
-        IntStream.range(0, b.length).forEach(i -> res[i] = b[i] * alpha);
+        for (int i = 0; i < b.length; i++) {
+            res[i] = b[i] * alpha;
+        }
         return res;
     }
 
     public double[] solve(SparseMatrix A, double[] f) {
         double[] x0 = new double[f.length];
         x0[0] = 1;
-        double[] r0 = subtractVectors(f, A.smartMultiplication(x0));
+        double[] r0 = subtract(f, A.smartMultiplication(x0));
         double[] z0 = r0;
         int MAX_ITERATIONS = 3000;
         for (int k = 1; k < MAX_ITERATIONS; k++) {
             double[] Az0 = A.smartMultiplication(z0);
             double alphaK = scalar(r0, r0) / scalar(Az0, z0);
 
-            double[] xK = sumVectors(x0, multiply(alphaK, z0));
-            double[] rK = subtractVectors(r0, multiply(alphaK, Az0));
+            double[] xK = sum(x0, mult(alphaK, z0));
+            double[] rK = subtract(r0, mult(alphaK, Az0));
 
 
             double betaK = scalar(rK, rK) / scalar(r0, r0);
-            double[] zK = sumVectors(rK, multiply(betaK, z0));
+            double[] zK = sum(rK, mult(betaK, z0));
             double epsilon = 0.00000000001;
             if (Math.sqrt(scalar(rK, rK) / scalar(f, f)) < epsilon) {
                 return xK;
